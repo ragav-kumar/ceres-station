4c8e0f7a1b4a5741cdda4a3204248062095db815
Ragav Kumar <27591832+ragav-kumar@users.noreply.github.com>
2025-03-09 19:14:49 -0700

    WIP: DB Initialization

---
+++ CeresStation.Context/Init/DatabaseInitializer.cs
@@ -0,0 +1,120 @@
+﻿using CeresStation.Model;
+
+namespace CeresStation.Core.Init;
+
+internal class DatabaseInitializer
+{
+    private readonly StationContext ctx;
+    private readonly Random random = new(1);
+
+    internal DatabaseInitializer(StationContext context)
+    {
+        ctx = context;
+    }
+
+    internal async Task Resources()
+    {
+        ctx.Add(new Resource { Id = Guid.NewGuid(), Name = "Iron" });
+        ctx.Add(new Resource { Id = Guid.NewGuid(), Name = "Copper" });
+        ctx.Add(new Resource { Id = Guid.NewGuid(), Name = "Silicates" });
+        ctx.Add(new Resource { Id = Guid.NewGuid(), Name = "Carbon" });
+        ctx.Add(new Resource { Id = Guid.NewGuid(), Name = "Water" });
+        ctx.Add(new Resource { Id = Guid.NewGuid(), Name = "Hydrogen" });
+        ctx.Add(new Resource { Id = Guid.NewGuid(), Name = "Oxygen" });
+        await ctx.SaveChangesAsync();
+        
+        // Not Listable, so no columns
+    }
+
+    internal async Task Extractors()
+    {
+        AddExtractor("Iron Mine", "Iron");
+        AddExtractor("Copper Mine", "Copper");
+        AddExtractor("Rock Quarry", "Silicates");
+        AddExtractor("Ice Mine", "Water");
+        await ctx.SaveChangesAsync();
+
+        // Initialize List columns
+        ctx.AddRange(
+            new Column
+            {
+                EntityType = EntityType.Extractor,
+                Order = 0,
+                FieldType = FieldType.Model,
+                DisplayName = "Name",
+                Width = 100,
+                FieldName = "Name",
+            },
+            new Column
+            {
+                EntityType = EntityType.Extractor,
+                FieldType = FieldType.Model,
+                Order = 1,
+                DisplayName = "Extraction Rate",
+                Width = 100,
+                FieldName = "ExtractionRate",
+            },
+            new Column
+            {
+                EntityType = EntityType.Extractor,
+                FieldType = FieldType.Model,
+                Order = 1,
+                DisplayName = "Standard Deviation",
+                Width = 100,
+                FieldName = "StandardDeviation",
+            },
+            new Column
+            {
+                EntityType = EntityType.Extractor,
+                FieldType = FieldType.Model,
+                Order = 1,
+                DisplayName = "Stockpile",
+                Width = 100,
+                FieldName = "Stockpile",
+            },
+            new Column
+            {
+                EntityType = EntityType.Extractor,
+                FieldType = FieldType.Model,
+                Order = 1,
+                DisplayName = "Capacity",
+                Width = 100,
+                FieldName = "Capacity",
+            }
+        );
+        await ctx.SaveChangesAsync();
+    }
+
+    private void AddExtractor(string extractorName, string resourceName)
+    {
+        Resource resource = ctx.Resources.Single(o => o.Name == resourceName);
+        ctx.Add(new Extractor
+        {
+            Id = Guid.NewGuid(),
+            Name = extractorName,
+            ResourceId = resource.Id,
+            ExtractionRate = 1.0f + (random.NextSingle() - 0.5f),
+            StandardDeviation = 0.1f + (random.NextSingle() - 0.5f) * 0.1f,
+            Stockpile = 0.0f,
+            Capacity = 100.0f + (random.NextSingle() - 0.5f) * 100f,
+        });
+    }
+
+    internal async Task Processors()
+    {
+        // TODO
+        await ctx.SaveChangesAsync();
+    }
+
+    internal async Task Consumers()
+    {
+        // TODO
+        await ctx.SaveChangesAsync();
+    }
+
+    internal async Task Transports()
+    {
+        // TODO
+        await ctx.SaveChangesAsync();
+    }
+}
--- CeresStation.Context/Program.cs
+++ CeresStation.Context/Program.cs
@@ -1,90 +1,29 @@
 ﻿using CeresStation.Core;
-using CeresStation.Model;
-using Microsoft.EntityFrameworkCore;
+using CeresStation.Core.Init;
 
-await using StationContext ctx = new StationContext();
+await using StationContext ctx = new();
 
 Console.WriteLine($"Database path: {ctx.DbPath}");
 
 // Initialize
-if (!ctx.Resources.Any(o => o.Name == "Iron"))
-{
-    ctx.Add(new Resource { Name = "Iron" });
-    await ctx.SaveChangesAsync();
-}
+DatabaseInitializer initialize = new(ctx);
 
-if (ctx.Extractors.Count() < 10)
-{
-    Resource iron = ctx.Resources.First();
-    Random random = new Random();
-    
-    for (int i = 0; i < 10; i++)
-    {
-        ctx.Add(new Extractor
-        {
-            ResourceId = iron.Id,
-            ExtractionRate = 1.0f + (random.NextSingle() - 0.5f),
-            StandardDeviation = 0.1f + (random.NextSingle() - 0.5f) * 0.1f,
-            Stockpile = 0.0f,
-            Capacity = 100.0f + (random.NextSingle() - 0.5f) * 100f,
-            Name = $"Extractor {i + 1}",
-        });
-    }
-    await ctx.SaveChangesAsync();
-}
+Console.WriteLine("Begin Database initialization.");
+Console.WriteLine("---------------------------------");
+Console.WriteLine("Deleting old database...");
+await ctx.Database.EnsureDeletedAsync();
+Console.WriteLine("Recreating database...");
+await ctx.Database.EnsureCreatedAsync();
 
-if (!ctx.Columns.Any(o => o.EntityType == EntityType.Extractor))
-{
-    ctx.AddRange(
-        new Column
-        {
-            EntityType = EntityType.Extractor,
-            Order = 0,
-            FieldType = FieldType.Model,
-            DisplayName = "Name",
-            Width = 100,
-            FieldName = "Name",
-        },
-        new Column
-        {
-            EntityType = EntityType.Extractor,
-            FieldType = FieldType.Model,
-            Order = 1,
-            DisplayName = "Extraction Rate",
-            Width = 100,
-            FieldName = "ExtractionRate",
-        },
-        new Column
-        {
-            EntityType = EntityType.Extractor,
-            FieldType = FieldType.Model,
-            Order = 1,
-            DisplayName = "Standard Deviation",
-            Width = 100,
-            FieldName = "StandardDeviation",
-        },
-        new Column
-        {
-            EntityType = EntityType.Extractor,
-            FieldType = FieldType.Model,
-            Order = 1,
-            DisplayName = "Stockpile",
-            Width = 100,
-            FieldName = "Stockpile",
-        },
-        new Column
-        {
-            EntityType = EntityType.Extractor,
-            FieldType = FieldType.Model,
-            Order = 1,
-            DisplayName = "Capacity",
-            Width = 100,
-            FieldName = "Capacity",
-        }
-    );
-    await ctx.SaveChangesAsync();
-}
+Console.WriteLine("Initializing resources...");
+await initialize.Resources();
+Console.WriteLine("Initializing extractors...");
+await initialize.Extractors();
+Console.WriteLine("Initializing processors...");
+await initialize.Processors();
+Console.WriteLine("Initializing consumers...");
+await initialize.Consumers();
+Console.WriteLine("Initializing transports...");
+await initialize.Transports();
 
-Extractor extractor = ctx.Extractors.Include(extractor => extractor.Resource).First();
-Console.WriteLine($"Extractor {extractor.Id} created: {extractor}");
-Console.WriteLine($"Resource: {extractor.Resource.Name}");
+Console.WriteLine("Database initialization complete.");
