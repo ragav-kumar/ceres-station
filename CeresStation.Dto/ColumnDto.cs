5303a6710539aa00459431bd00f8debe14ccfdf5
Ragav Kumar <Ragav.Kumar@subnet.com>
2025-03-23 19:40:02 -0600

    Implement PUT for columns

--- CeresStation.Dto/ColumnDto.cs
+++ CeresStation.Dto/ColumnDto.cs
@@ -3,11 +3,11 @@
 namespace CeresStation.Dto;
 
 public record ColumnDto(
-	Guid Id,
-	EntityType EntityType,
+	Guid? Id,
+	EntityType? EntityType,
 	string? DisplayName,
 	Guid? AttributeDefinitionId,
 	string? FieldName,
-	int Width,
-	int Order
+	int? Width,
+	int? Order
 );
